62c7c0584cbe229b23e0c347157de1f5ef02a228
Orangine <127820157+Orangine@users.noreply.github.com>
2025-12-19 01:26:42 -0300

    Init collections

---
+++ data/collections/orangine.ts
@@ -0,0 +1,11 @@
+import { Collection } from '../../types';
+
+export const collections: Collection[] = [
+  {
+    "id": "winds-of-china",
+    "title": "Winds of China",
+    "description": "Uma viagem visual pelas paisagens de Where Winds Meet.",
+    "coverImageId": "PM-DECJ3P",
+    "imageIds": [ "PM-DECJ3P", "PM-83X8DP", "PM-0MOWYG", "PM-GQF9C4", "PM-BTCGUD", "PM-UJB1IV", "PM-BOOR3P", "PM-Z7Y9ZR" ]
+  }
+];
