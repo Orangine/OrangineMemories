7c2ae5073b7bea75ead702df13967ec0756ee0f8
Orangine <127820157+Orangine@users.noreply.github.com>
2025-12-19 02:07:15 -0300

    Update collections (1)

--- collections.ts
+++ collections.ts
@@ -5,7 +5,7 @@ export const collections: Collection[] = [
     "id": "winds-of-china",
     "title": "Winds of China",
     "description": "Uma viagem visual pelas paisagens de Where Winds Meet.",
-    "coverImageId": "PM-83X8DP",
-    "imageIds": [ "PM-DECJ3P", "PM-83X8DP", "PM-0MOWYG", "PM-GQF9C4", "PM-BTCGUD", "PM-UJB1IV", "PM-BOOR3P", "PM-Z7Y9ZR" ]
+    "coverImageId": "PM-BOOR3P",
+    "imageIds": [ "PM-DECJ3P", "PM-83X8DP", "PM-0MOWYG", "PM-GQF9C4", "PM-BTCGUD", "PM-UJB1IV", "PM-BOOR3P", "PM-Z7Y9ZR", "PM-X2VBUD" ]
   }
 ];
